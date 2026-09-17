556a2340c7bff11a2b6a118b4e48e300f1c50e94
Alexander Guller <alex.1guller@gmail.com>
2026-09-17 18:43:32 -0400

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # binary search on max hours
+        rt = max(piles)
+        lt = 1
+        mh = sys.maxsize
+        while lt <= rt:
+            mid = (rt + lt) // 2
+            hours = 0
+            for i in range(len(piles)):
+                hours += math.ceil(piles[i] / mid)
+            
+            if hours <= h:
+                mh = min(mh, mid)
+                rt = mid - 1
+            else:
+                lt = mid + 1
+        return mh
